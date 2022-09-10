593dfa7da87c45742dffab822eb93aefc3d2e9f7
Harits Wahid Vijayanto <haritswahid99@gmail.com>
2022-09-11 02:57:47 +0700

    Mengaplikasikan validator

--- src/api/notes/handler.js
+++ src/api/notes/handler.js
@@ -1,6 +1,7 @@
 class NotesHandler {
-  constructor(service) {
+  constructor(service,validator) {
     this.service = service;
+    this.validator = validator;
  
     this.postNoteHandler = this.postNoteHandler.bind(this);
     this.getNotesHandler = this.getNotesHandler.bind(this);
@@ -11,6 +12,7 @@ class NotesHandler {
 
   postNoteHandler(request, h) {
     try {
+      this.validator.validateNotePayload(request.payload);
       const { title = 'untitled', body, tags } = request.payload;
 
       const noteId = this.service.addNote({ title, body, tags });
@@ -66,6 +68,7 @@ class NotesHandler {
 
   putNoteByIdHandler(request, h) {
     try {
+      this.validator.validateNotePayload(request.payload);
       const { id } = request.params;
 
       this.service.editNoteById(id, request.payload);
