d1d9605257aa529ca09683256511729d4cf3a64d
Harits Wahid Vijayanto <haritswahid99@gmail.com>
2022-09-12 10:59:47 +0700

    Inisiasi plugin notes v.2

--- src/api/notes/index.js
+++ src/api/notes/index.js
@@ -3,7 +3,7 @@ const routes = require('./routes');
 
 module.exports = {
   name: 'notes',
-  version: '1.0.0',
+  version: '2.0.0',
   register: async (server, { service, validator }) => {
     const notesHandler = new NotesHandler(service, validator);
     server.route(routes(notesHandler));
